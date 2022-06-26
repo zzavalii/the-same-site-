0e3e8fdc6e3b76d3b3a6778096876a5a6f9c6a7b
Vlados <93836670+zzavalii@users.noreply.github.com>
2022-06-26 18:02:24 +0300

    Update app.js
    me

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('config');
-const PORT =config.get('port') || 5000;
-const User = require('./models/User');
 
-const app = express();
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const config = require('config'); 
+const User = require('./models/User'); 
+const PORT = config.get('port') || 5000; 
+ 
+const app = express(); 
+ 
+app.use(require('cors')({origin: '*'})); 
+app.use(express.urlencoded());
 
-async function start() {
-    try{
-         await mongoose.connect(config.get('mongoURI'))
-         app.listen(5000, () => { console.log(`App has been started ${PORT}`)});
-     }
-    catch(e) {
-        console.log(e);
-    }
-}
-start();
-
-app.get('/users', async (req, res) => {
-    try{
+ 
+async function start() { 
+    try { 
+        await mongoose.connect(config.get('mongoURI')); 
+        app.listen(PORT, () => { console.log(`App has been started on port ${PORT}`) }); 
+    } 
+    catch(e) { 
+        console.log(e); 
+    } 
+} 
+ 
+start(); 
+ 
+app.get('/users', async (req, res) => { 
+    try { 
         let users = await User.find(); 
-        res.json(users);
-    }
-    catch(e){
-      console.log(e);
-      res.status(500).send('Internal Server Error');
-    }
-  
+        res.json(users); 
+    } 
+    catch(e) { 
+        console.log(e); 
+        res.status(500).send('Internal Server Error'); 
+    } 
+})
+
+
+app.post('/register', async (req, res) => {
+   try{
+    let user = new User(req.body);
+    await user.save();
+    res.redirect("http://localhost:3000/dating")
+   }
+   catch(e){
+     console.log(e);
+     res.redirect("http://localhost:3000/dating")
+   }
 })
