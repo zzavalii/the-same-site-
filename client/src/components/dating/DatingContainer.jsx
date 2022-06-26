0d3ef901d76ce8f2fbd83efd0b8f22fadacdd676
Vlados <93836670+zzavalii@users.noreply.github.com>
2022-06-26 18:04:53 +0300

    Update DatingContainer.jsx
    me

--- client/src/components/dating/DatingContainer.jsx
+++ client/src/components/dating/DatingContainer.jsx
@@ -1,23 +1,31 @@
-import { connect } from "react-redux";
-import { setDatingActionCreator } from "../../redux/datingReducer";
-import Dating from "./Dating";
-
-
-let mapStateToProps = (state) => {
-    return{
-       users: state.datingState.users
-    }
-}
-
-let mapDispatchToProps = (dispatch) => {
-    return{
-      date: (id) => { dispatch(setDatingActionCreator(id, true)) },
-      unDate: (id) => { dispatch(setDatingActionCreator(id, false)) }
-    }
-}
-
-
-const DatingContainer = connect(mapStateToProps, mapDispatchToProps)(Dating)
-
-
-export default DatingContainer;
+import { connect } from 'react-redux'; 
+import Dating from './Dating'; 
+import { setDatingActionCreator, getUsersThunkCreator } from './../../redux/datingReducer'; 
+import { Component } from 'react'; 
+ 
+let mapStateToProps = (state) => { 
+    return { 
+        users: state.datingState.users 
+    } 
+} 
+ 
+let mapDispatchToProps = (dispatch) => { 
+    return { 
+        date: (id) => { dispatch(setDatingActionCreator(id, true)) }, 
+        unDate: (id) => { dispatch(setDatingActionCreator(id, false)) }, 
+        fetchData: () => { dispatch(getUsersThunkCreator()) }  
+    } 
+} 
+ 
+class DatingContainer extends Component { 
+ 
+    componentDidMount() { 
+        this.props.fetchData(); 
+    } 
+ 
+    render() { 
+        return <Dating {...this.props} /> 
+    } 
+}  
+ 
+export default connect(mapStateToProps,mapDispatchToProps)(DatingContainer);
