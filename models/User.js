bd8a13eea91e690e296307552559dd9aee2871ab
Vlados <93836670+zzavalii@users.noreply.github.com>
2022-06-26 18:03:00 +0300

    Update User.js
    me

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const{ model, Schema } = require('mongoose');
 
 const User = new Schema({
     login: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
+    password: { type: String, required: true},
+    photoURL: {type: String},
+    age: {type:String},
+    gender: {type: String},
+    name: {type: String}
 })
 
 module.exports = model('user', User);
