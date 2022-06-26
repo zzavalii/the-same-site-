c5e2338f3752b0ce67da1b16cc915311a78494fa
Vlados <93836670+zzavalii@users.noreply.github.com>
2022-06-26 18:03:58 +0300

    Update Dating.jsx
    me

--- client/src/components/dating/Dating.jsx
+++ client/src/components/dating/Dating.jsx
@@ -7,12 +7,24 @@ const Dating = (props) => {
     <img wigth="100"src={u.photoURL} alt="ava" />
     <h3>{u.name}</h3>
     <p>Age: {u.age} Gender: {u.gender}</p>
+    <p>Login: {u.login} | Password: {u.password}</p>
   </div>
   );
     return(
+      <div>
       <div className={classes.dd}>
         {users}
       </div>
+     <form action="http://localhost:5000/register" method="POST">
+      <input type="text" name="login" placeholder="login" />
+      <input type="text" name="password" placeholder="password" />
+      <input type="text" name="name" placeholder="name" />
+      <input type="text" name="age" placeholder="age" />
+      <input type="text" name="gender" placeholder="gender" />
+      <input type="submit"/>
+      
+      </form>
+       </div>
     );
 }
 
